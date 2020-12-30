c3ea693daee8a960f57a35a261305efccdadd2de
mmtakeuchi <mikemtaki@gmail.com>
2020-12-30 15:33:56 -0800

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :show, :index]
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
 
-  post '/users', to: 'users#create'
-  get '/users/:user_id', to: 'users#show'
-  get '/users', to: 'users#index'
+  resources :users, only: [:create, :show, :index] do 
+      resources :items, only: [:create, :show, :index, :destroy]
+  end
 end
