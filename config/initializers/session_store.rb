18e4d705bd7da5ba9d98f5f3f16b9847bc1b7835
mmtakeuchi <mikemtaki@gmail.com>
2021-04-25 21:41:28 -0700

    change domain to :all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_wine-journal_server', domain: 'https://wine-journal-client.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, key: '_wine-journal-server', domain: :all
 else
-    Rails.application.config.session_store :cookie_store, key: '_wine-journal_server'
+    Rails.application.config.session_store :cookie_store, key: '_wine-journal-server'
 end
